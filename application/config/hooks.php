61ef2816abc52b058abeb0498ab4ec9328203878
小周 <slps970093@gmail.com>
2022-08-24 23:58:40 +0800

    [tool] 安裝 debugger bar

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/userguide3/general/hooks.html
 |
 */
+
+$hook['display_override'][] = array(
+	'class'  	=> 'Develbar',
+	'function' 	=> 'debug',
+	'filename' 	=> 'Develbar.php',
+	'filepath' 	=> 'third_party/DevelBar/hooks'
+);
